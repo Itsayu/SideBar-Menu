106a5b437cc8cf8f630cf5117746475888221ef6
Ayush Kumar <94097145+Itsayu@users.noreply.github.com>
2023-04-10 00:32:34 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let menu = document.querySelector('#menu-icon');
+let sidenavbar = document.querySelector('.side-navbar');
+let content = document.querySelector('.content');
+ 
+menu.onclick = () => {
+    sidenavbar.classList.toggle('active');
+    content.classList.toggle('active');
+}
